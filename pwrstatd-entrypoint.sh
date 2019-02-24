124d2b88f8018fdc3ade37c1308bb6777edfe3a6
Jesse Newland <jesse@jnewland.com>
2019-02-24 14:43:53 -0600

    trap

--- pwrstatd-entrypoint.sh
+++ pwrstatd-entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 cp -Lr /config/pwrstatd* /etc/
-/usr/local/sbin/pwrstatd&
+/usr/local/sbin/pwrstatd &
+pid=$!
+trap "kill $pid; exit" TERM
 
 while true; do
+  sleep 10
   # TODO parse and send to pushgateway
   # https://github.com/prometheus/pushgateway#command-line
   /usr/local/sbin/pwrstat -status
-  sleep 30
+  sleep 10 && sleep 10
 done
